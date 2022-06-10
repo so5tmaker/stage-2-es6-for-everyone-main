a82122580f40e741f9d8c9adc4ce58495e30898c
Viktor Sivulskiy <info@softmaker.kz>
2022-06-10 07:42:11 +0600

    modal styles were changed

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -20,6 +20,16 @@ export async function fight(firstFighter, secondFighter) {
         isCriticalHit: true,
         isBlock: false,
         ...secondFighter
+      },
+      setCriticalHit(fighter) {
+        setTimeout(() => fighter.isCriticalHit = true, 10000);
+      },
+      setHealthIndicator(currentlyHealth, fighterHealth) {
+        const indicator = currentlyHealth * 100 / fighterHealth;
+        return indicator > 0 ? `${indicator}%` : 0;
+      },
+      getCriticalHit(fighter) {
+        return fighter.attack * 2;
       }
     };
 
@@ -33,35 +43,43 @@ export async function fight(firstFighter, secondFighter) {
       pressedKeys.delete(e.code);
       switch (e.code) {
         case PlayerOneBlock:
-          fightersOptions.leftFighter.isBlock = false;
+          leftFighter.isBlock = false;
           break;
         case PlayerTwoBlock:
-          fightersOptions.rightFighter.isBlock = false;
+          rightFighter.isBlock = false;
           break;
         default:
           break;
       }
     }
 
+    const {
+      leftFighter,
+      rightFighter,
+      getCriticalHit,
+      setHealthIndicator,
+      setCriticalHit
+    } = fightersOptions;
+
     const handleKeyPress = (e) => {
       pressedKeys.add(e.code);
 
       switch (e.code) {
         case PlayerOneAttack:
-          if (!fightersOptions.leftFighter.isBlock) {
-            fightersOptions.rightFighter.health -= getDamage(fightersOptions.leftFighter, fightersOptions.rightFighter);
+          if (!leftFighter.isBlock) {
+            rightFighter.health -= getDamage(leftFighter, rightFighter);
           }
           break;
         case PlayerOneBlock:
-          fightersOptions.leftFighter.isBlock = true;
+          leftFighter.isBlock = true;
           break;
         case PlayerTwoAttack:
-          if (!fightersOptions.rightFighter.isBlock) {
-            fightersOptions.leftFighter.health -= getDamage(fightersOptions.rightFighter, fightersOptions.leftFighter);
+          if (!rightFighter.isBlock) {
+            leftFighter.health -= getDamage(rightFighter, leftFighter);
           }
           break;
         case PlayerTwoBlock:
-          fightersOptions.rightFighter.isBlock = true;
+          rightFighter.isBlock = true;
           break;
         default:
           break;
@@ -72,25 +90,25 @@ export async function fight(firstFighter, secondFighter) {
           .every((code) => pressedKeys.has(code)) ? 'leftFighter' : 'rightFighter';
         switch (playerCriticalHit) {
           case 'leftFighter':
-            if (fightersOptions.leftFighter.isCriticalHit && !fightersOptions.leftFighter.isBlock) {
-              fightersOptions.rightFighter.health -= getCriticalHit(fightersOptions.leftFighter);
-              fightersOptions.leftFighter.isCriticalHit = false;
-              setCriticalHit(fightersOptions.leftFighter);
+            if (leftFighter.isCriticalHit && !leftFighter.isBlock) {
+              rightFighter.health -= getCriticalHit(leftFighter);
+              leftFighter.isCriticalHit = false;
+              setCriticalHit(leftFighter);
             }
             break;
           case 'rightFighter':
-            if (fightersOptions.rightFighter.isCriticalHit && !fightersOptions.rightFighter.isBlock) {
-              fightersOptions.leftFighter.health -= getCriticalHit(fightersOptions.rightFighter);
-              fightersOptions.rightFighter.isCriticalHit = false;
-              setCriticalHit(fightersOptions.rightFighter);
+            if (rightFighter.isCriticalHit && !rightFighter.isBlock) {
+              leftFighter.health -= getCriticalHit(rightFighter);
+              rightFighter.isCriticalHit = false;
+              setCriticalHit(rightFighter);
             }
             break;
           default:
             break;
         }
       }
-      const healthIndicatorPlayerOne = setHealthIndicator(fightersOptions.leftFighter.health, firstFighter.health);
-      const healthIndicatorPlayerTwo = setHealthIndicator(fightersOptions.rightFighter.health, secondFighter.health);
+      const healthIndicatorPlayerOne = setHealthIndicator(leftFighter.health, firstFighter.health);
+      const healthIndicatorPlayerTwo = setHealthIndicator(rightFighter.health, secondFighter.health);
 
       leftHealthIndicator.style.width = healthIndicatorPlayerOne;
       rightHealthIndicator.style.width = healthIndicatorPlayerTwo;
@@ -108,24 +126,11 @@ export async function fight(firstFighter, secondFighter) {
   });
 }
 
-export function setHealthIndicator(currentlyHealth, basedHealth) {
-  const indicator = currentlyHealth * 100 / basedHealth;
-  return indicator > 0 ? `${indicator}%` : 0;
-}
-
-export function setCriticalHit(player) {
-  setTimeout(() => player.isCriticalHit = true, 10000);
-}
-
-export function getCriticalHit(fighter) {
-  return fighter.attack * 2;
-}
-
 export function getDamage(attacker, defender) {
   // return damage
-  const damage = getHitPower(attacker) - getBlockPower(defender)
-  const isBlockedDamage = defender.isBlock && defender.defense > attacker.attack;
-  return isBlockedDamage || Math.sign(damage) === -1 ? 0 : damage;
+  const { isBlock } = defender;
+  const damage = getHitPower(attacker) - getBlockPower(defender);
+  return damage <= 0 || isBlock ? 0 : damage;
 }
 
 export function getHitPower(fighter) {
